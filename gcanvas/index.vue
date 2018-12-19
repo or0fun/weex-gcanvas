c18b83a1262b243bd9d77b2d516c1c44c77dc12b
JunFang <fangjun.nk@gmail.com>
2018-12-19 10:10:18 +0800

    Create index.vue

---
+++ gcanvas/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrapper">
+    <scroller style="flex: 1; width: 750px;">
+      <pie-chart></pie-chart>
+      <bar-chart></bar-chart>
+      <line-chart></line-chart>
+    </scroller>
+  </div>
+</template>
+<script>
+import BarChart from './components/bar-chart'
+import LineChart from './components/line-chart'
+import PieChart from './components/pie-chart'
+const navigator = weex.requireModule('navigator')
+
+export default {
+  data () {
+    return {
+      chart: null,
+      timeStamp: 0
+    }
+  },
+  components: { PieChart, LineChart, BarChart },
+  methods: {
+    leftClick () {
+      navigator.pop()
+    }
+  }
+}
+</script>
+<style scoped>
+  .wrapper {
+    width: 750px;
+    background-color: white;
+  }
+</style>
